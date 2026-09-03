20dae0887a43367e577edcc9549fc85d8accd196
NColeGit2 <dukedoodooiv@gmail.com>
2026-09-03 10:49:51 -0400

    new sketch for physical led

---
+++ folder1/sketch_sep03c/sketch_sep03c/sketch_sep03c.ino
@@ -0,0 +1,14 @@
+const int led = 2;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("LED ON");
+  digitalWrite(led, LOW);
+  delay(500);
+}
