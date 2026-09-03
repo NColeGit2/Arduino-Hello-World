b9cc4ce0fc89aa96246ffda4946fdf2f15ae5ea0
NColeGit2 <dukedoodooiv@gmail.com>
2026-09-03 11:14:17 -0400

    Fixed the code for c

--- folder1/sketch_sep03c/sketch_sep03c/sketch_sep03c.ino
+++ folder1/sketch_sep03c/sketch_sep03c/sketch_sep03c.ino
@@ -9,6 +9,10 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   Serial.println("LED ON");
+  digitalWrite(led, HIGH);
+  delay(1000);
+  
+  Serial.println("LED OFF");
   digitalWrite(led, LOW);
   delay(500);
 }
